01302debf5d323a051928bca29a7dd805fc89789
Anton-arch <gats9@yandex.ru>
2021-05-27 13:33:53 +0300

    Scroll on second slide

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
+"use strict";
+
 const slider = document.querySelector('.swiper');
 const slides = Array.from(document.querySelectorAll('.swiper__slide'));
 const slideWidth = document.querySelector('.swiper__slide').clientWidth;
 const homePageLink = document.querySelector('.home-page__link');
 const whatsNextLink = document.querySelector('.slide-first__link-next');
-
 let isDragging = false;
 let startPos = 0;
 let currentTranslate = 0;
 let prevTranslate = 0;
 let animationID = 0;
 let currentIndex = 0;
-
 slides.forEach((slide, index) => {
     // Touch events
     slide.addEventListener('touchstart', touchStart(index));
     slide.addEventListener('touchend', touchEnd);
-    slide.addEventListener('touchmove', touchMove);
+    slide.addEventListener('touchmove', touchMove); // Mouse events
 
-    // Mouse events
     slide.addEventListener('mousedown', touchStart(index));
     slide.addEventListener('mouseup', touchEnd);
     slide.addEventListener('mouseleave', touchEnd);
     slide.addEventListener('mousemove', touchMove);
-});
+}); // prevets context menu
 
-// prevets context menu
 window.oncontextmenu = function (event) {
     event.preventDefault();
     event.stopPropagation();
@@ -36,7 +34,6 @@ function touchStart(index) {
         currentIndex = index;
         startPos = getPositionX(event);
         isDragging = true;
-
         animationID = requestAnimationFrame(animation);
         slider.classList.add('grabbing');
     };
@@ -45,7 +42,6 @@ function touchStart(index) {
 function touchEnd() {
     isDragging = false;
     cancelAnimationFrame(animationID);
-
     const movedBy = currentTranslate - prevTranslate;
 
     if (movedBy < -100 && currentIndex < slides.length - 1) {
@@ -57,7 +53,6 @@ function touchEnd() {
     }
 
     setPositionByIndex();
-
     slider.classList.remove('grabbing');
 }
 
@@ -74,6 +69,7 @@ function getPositionX(event) {
 
 function animation() {
     setSliderPosition();
+
     if (isDragging) {
         requestAnimationFrame(animation);
     }
@@ -82,15 +78,13 @@ function animation() {
 function setSliderPosition() {
     slider.style.transform = `translateX(${currentTranslate}px)`;
     slider.style.transition = 'transform 0.3s ease-out';
-
     document.querySelectorAll('.layer-3-copy').forEach(item => {
         item.classList.add('active');
-    })
+    });
 }
 
 function setPositionByIndex() {
     currentTranslate = currentIndex * -slideWidth;
-
     prevTranslate = currentTranslate;
     setSliderPosition();
 }
@@ -99,54 +93,46 @@ homePageLink.addEventListener('click', () => {
     currentIndex = 0;
     setPositionByIndex();
 });
-
 whatsNextLink.addEventListener('click', () => {
     currentIndex = 1;
     setPositionByIndex();
 });
-
 document.querySelector('.slide-third__text-container_more-datails-link').addEventListener('click', () => {
     document.querySelector('.slide-third__modal_overlay').classList.remove('visually-hidden');
 });
-
-document.querySelector('.slide-third__modal_close').addEventListener('click', (e) => {
+document.querySelector('.slide-third__modal_close').addEventListener('click', e => {
     document.querySelector('.slide-third__modal_overlay').classList.add('visually-hidden');
 });
-
 const listsItems = document.querySelectorAll('.slide-third__modal-swiper_slide-item');
 let i = 0;
-
 listsItems.forEach(item => {
-        i++
-        let count = document.createElement('div');
-        count.style.fontFamily = 'Gilroy';
-        count.style.color = '#8cc9e8';
-        count.style.fontWeight = '600';
-        count.style.marginBottom = '6px';
-        
-        if(i < 10) {
-            count.textContent = `0${i}`;
-            item.prepend(count);
-        } else {
-            count.textContent = `${i}`;
-            item.prepend(count);
-        }
-});
+    i++;
+    let count = document.createElement('div');
+    count.style.fontFamily = 'Gilroy';
+    count.style.color = '#8cc9e8';
+    count.style.fontWeight = '600';
+    count.style.marginBottom = '6px';
 
+    if (i < 10) {
+        count.textContent = `0${i}`;
+        item.prepend(count);
+    } else {
+        count.textContent = `${i}`;
+        item.prepend(count);
+    }
+});
 const modalSwiper = document.querySelector('.slide-third__modal-swiper');
 const modalDots = document.querySelectorAll('.dot');
 const modalSlides = document.querySelectorAll('.slide-third__modal-swiper_slide');
-    
 let modalCurrentIndex = 0;
-
 const prevButton = document.querySelector('.previous');
 const nextButton = document.querySelector('.next');
 
-nextButton.onclick = function() {
+nextButton.onclick = function () {
     nextSlide();
 };
 
-prevButton.onclick = function() {
+prevButton.onclick = function () {
     previousSlide();
 };
 
@@ -161,7 +147,31 @@ function previousSlide() {
 function goToSlide(n) {
     modalDots[modalCurrentIndex].className = 'dot';
     modalSlides[modalCurrentIndex].className = 'slide-third__modal-swiper_slide';
-    modalCurrentIndex = (n+modalSlides.length)%modalSlides.length;
+    modalCurrentIndex = (n + modalSlides.length) % modalSlides.length;
     modalSlides[modalCurrentIndex].className = 'slide-third__modal-swiper_slide modal-active-slide';
     modalDots[modalCurrentIndex].className = 'dot modal-active-dot';
 }
+
+let currentPosition;
+const scroll = document.querySelector('.slide-second__scrollblock');
+const viewport = document.querySelector('.slide-second__scrollbar_text');
+const textBox = document.querySelector('.slide-second__scrollbar_text-container');
+const scrollBar = document.querySelector('.slide-second__scrollblock_scrollbar');
+const scroller = document.querySelector('.slide-second__scrollblock_scroller'); //Соотношение между размером окна в котором будет крутиться текст и размером самого текста
+//нужно для динамического определения размера ползунка
+
+const ratio = viewport.clientHeight / textBox.scrollHeight;
+scroller.style.height = ratio * viewport.clientHeight + 'px'; // console.log(scrollBar.clientHeight)
+
+function setCurrentPosition(event) {
+    currentPosition = event.changedTouches[0].clientY - event.changedTouches[0].target.clientHeight;
+    scroller.style.top = currentPosition + 'px';
+    textBox.scrollTop = currentPosition / ratio;
+}
+
+scroller.addEventListener('touchstart', setCurrentPosition);
+scroller.addEventListener('touchmove', setCurrentPosition);
+scroller.addEventListener('touchend', setCurrentPosition);
+textBox.addEventListener('scroll', () => {
+    scroller.style.top = textBox.scrollTop * ratio + 'px';
+});
